f839e3e7a181586a861908d7766d14e3137b18b2
lautaromateol <lautaromateol@gmail.com>
2024-05-18 18:48:22 -0300

    loading spinner created

---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-white">
+    <div className="h-16 w-16 animate-spin rounded-full border-2 border-solid border-shade border-t-transparent"></div>
+  </div>
+  )
+}
