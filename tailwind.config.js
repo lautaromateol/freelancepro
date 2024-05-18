be9349509bcd16a1dd8d986db0fb4de7044891a9
lautaromateol <lautaromateol@gmail.com>
2024-05-18 14:32:37 -0300

    new color variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(79 70 229)",
+        tint: "rgb(49 46 129)",
+        shade: "rgb(129 140 248)",
+        danger: "rgb(239 68 68)"
+      }
+    },
   },
   plugins: [],
 }
