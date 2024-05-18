8eb25c48968fb5efbf7050cc8ae82fd5247b1eff
lautaromateol <lautaromateol@gmail.com>
2024-05-18 18:51:48 -0300

    page styling reusable components

---
+++ src/ui/PageHeader.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export default function PageHeading({ children }) {
+  return (
+    <h2 className="font-bold text-5xl mb-4">
+      {children}
+    </h2>
+  )
+}
