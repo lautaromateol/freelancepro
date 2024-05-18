e38900421dd4915dd972376e3a178ee97545f69a
lautaromateol <lautaromateol@gmail.com>
2024-05-18 14:34:02 -0300

    tasks route removed and queryclient provider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import AppLayout from "./ui/AppLayout"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Project from "./pages/Project"
 import Task from "./pages/Task"
-import Tasks from "./pages/Tasks"
 import NotFound from "./ui/NotFound"
 
 function App() {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0
+      }
+    }
+  })
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate to="dashboard"/>} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="project/:projectId" element={<Project />} />
-          <Route path="tasks" element={<Tasks />} />
-          <Route path="task/:taskId" element={<Task />} />
-        </Route>
-        <Route path="*" element={<NotFound />}/>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="project/:projectId" element={<Project />} />
+            <Route path="task/:taskId" element={<Task />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
