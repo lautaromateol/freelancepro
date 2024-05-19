ab13cef3366758a6305f04ab42e4d4511f9b8b62
lautaromateol <lautaromateol@gmail.com>
2024-05-19 17:29:10 -0300

    props added to button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/prop-types */
-export default function Button({children, onClick, type, disabled}) {
+export default function Button({ children, onClick, type, disabled = false, size = "normal", flex = false }) {
   return (
-    <button disabled={disabled} type={type} className="p-4 text-slate-50 bg-primary text-2xl font-medium rounded-lg w-full hover:bg-tint transition-colors" onClick={onClick}>
+    <button
+      disabled={disabled}
+      type={type}
+      className={`${flex && "flex items-center justify-center gap-2"} ${size === "normal" ? "py-4 px-2" : "p-2"} text-slate-50 bg-primary text-2xl font-medium rounded-lg w-full hover:bg-tint transition-colors`}
+      onClick={onClick}>
       {children}
     </button>
   )
