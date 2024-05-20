13f228814c6a51783b02cbcdf157478b493e0ccd
lautaromateol <lautaromateol@gmail.com>
2024-05-20 12:40:58 -0300

    date formating function created

---
+++ src/utils/helpers.js
@@ -0,0 +1,11 @@
+export function formatDate(date) {
+  const endDate = new Date(date)
+  const day = endDate.getDate()
+  const month = endDate.getMonth() + 1
+  const year = endDate.getFullYear()
+  const formatedDay = day.toString().padStart(2, "0")
+  const formatedMonth = month.toString().padStart(2, "0")
+
+  return`${formatedDay}/${formatedMonth}/${year}`
+
+}
