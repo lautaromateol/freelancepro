de8bc381eeabfb19e3e242e21612d3aac707bb9f
lautaromateol <lautaromateol@gmail.com>
2024-05-20 12:38:45 -0300

    paginate and filters added to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,26 +5,46 @@ import PageSubHeading from "../ui/PageSubHeading"
 import ProjectChart from "../features/projects/ProjectChart"
 import Spinner from "../ui/Spinner"
 import Paginate from "../ui/Paginate"
+import Filters from "../ui/Filters"
+import { useSearchParams } from "react-router-dom"
+
+const options = [
+  {
+    label: "In Progress",
+    value: "in-progress"
+  },
+  {
+    label: "Complete",
+    value: "complete"
+  },
+]
 
 export default function Projects() {
 
   const { projects, isPending, count } = useProjects()
 
+  const [searchParams] = useSearchParams()
+
+  const statusParam = searchParams.get("status") || options.at(0).value
+
+  const status = statusParam.split("-").length > 1 ? statusParam.split("-").map((word) => word.at(0).toUpperCase() + word.slice(1)).join(" ") : statusParam.at(0).toUpperCase() + statusParam.slice(1)
+
+  const filteredProjects = projects?.filter((project) => project.status === status)
+
   if (isPending) return <Spinner />
+
 
   return (
     <PageSection>
       <div className="flex items-center justify-between">
         <div>
-        <PageHeading>Your Projects</PageHeading>
-        <PageSubHeading>A summary of your active projects and key statistics.</PageSubHeading>
+          <PageHeading>Your Projects</PageHeading>
+          <PageSubHeading>A summary of your active projects and key statistics.</PageSubHeading>
         </div>
-        <div>
-          Filters
-        </div>
+        <Filters options={options} />
       </div>
       <div className="grid grid-cols-3 gap-6">
-        {projects.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectChart project={project} key={project.id} />
         ))}
       </div>
