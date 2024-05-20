ec6717129eb869cd400645f0dbf5df26c944b063
lautaromateol <lautaromateol@gmail.com>
2024-05-20 12:39:25 -0300

    page heading styling changes

--- src/ui/PageHeader.jsx
+++ src/ui/PageHeader.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function PageHeading({ children }) {
   return (
-    <h2 className="font-bold text-5xl mb-4">
+    <h2 className="font-semibold text-5xl mb-4">
       {children}
     </h2>
   )
