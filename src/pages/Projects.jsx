3988d90273cfc13d4516890038d6326dd6f2d062
lautaromateol <lautaromateol@gmail.com>
2024-05-23 00:06:05 -0300

    new message at projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,7 @@
 import { useProjects } from "../features/projects/useProjects"
+import { useSearchParams } from "react-router-dom"
+import { HiOutlineFolderOpen } from "react-icons/hi"
+import CreateProject from "../features/projects/CreateProject"
 import PageSection from "../ui/PageSection"
 import PageHeading from "../ui/PageHeader"
 import PageSubHeading from "../ui/PageSubHeading"
@@ -6,7 +9,6 @@ import ProjectChart from "../features/projects/ProjectChart"
 import Spinner from "../ui/Spinner"
 import Paginate from "../ui/Paginate"
 import Filters from "../ui/Filters"
-import { useSearchParams } from "react-router-dom"
 
 const options = [
   {
@@ -33,21 +35,31 @@ export default function Projects() {
 
   if (isPending) return <Spinner />
 
+  if (!projects.length) return (
+      <section className="flex items-center justify-center">
+        <div className="rounded-lg bg-slate-50 p-8 max-w-[40rem] border-2">
+          <HiOutlineFolderOpen className="mx-auto size-[5rem] mb-6" />
+          <h4 className="text-4xl font-bold mb-6 text-center">You don&apos;t have any project!</h4>
+          <p className="text-2xl mb-6 text-gray-500 text-center">It seems that you don&apos;t have created any project yet. Start working in something new today!</p>
+          <CreateProject />
+        </div>
+      </section>
+  )
 
   return (
     <PageSection>
-      <div className="flex items-center justify-between">
+      <header className="flex items-center justify-between">
         <div>
           <PageHeading>Your Projects</PageHeading>
           <PageSubHeading>A summary of your active projects and key statistics.</PageSubHeading>
         </div>
         <Filters options={options} />
-      </div>
-      <div className="grid grid-cols-3 gap-6">
+      </header>
+      <section className="grid grid-cols-3 gap-6">
         {filteredProjects.map((project) => (
           <ProjectChart project={project} key={project.id} />
         ))}
-      </div>
+      </section>
       <Paginate count={count} />
     </PageSection>
   )
