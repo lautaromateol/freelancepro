b7dfa9a1ff56b28ac5ea5b0b908ee166498621f2
lautaromateol <lautaromateol@gmail.com>
2024-05-23 00:00:05 -0300

    no budget option implemented

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import { useForm } from "react-hook-form"
+import { useCreateProject } from "./useCreateProject"
 import Form from "../../ui/Form"
 import FormRow from "../../ui/FormRow"
 import Label from "../../ui/Label"
 import Input from "../../ui/Input"
 import Textarea from "../../ui/Textarea"
 import Button from "../../ui/Button"
-import { useCreateProject } from "./useCreateProject"
 
 export default function CreateProjectForm({ onCloseModal }) {
+
+  const [checked, setChecked] = useState(false)
 
   const { formState, register, handleSubmit } = useForm()
 
@@ -21,6 +24,7 @@ export default function CreateProjectForm({ onCloseModal }) {
       ...data,
       startDate: new Date(data.startDate),
       finishDate: new Date(data.finishDate),
+      budget: checked ? 0 : data.budget,
       user_id: 1
     }
     createProject(newProject, {
@@ -35,14 +39,26 @@ export default function CreateProjectForm({ onCloseModal }) {
         <span className="text-3xl font-thin text-tint">Fill out the details to create a new project.</span>
         <FormRow>
           <Label htmlFor="name">Project Name</Label>
-          <Input type="text" id="name" register={register} disabled={isPending} />
+          <Input
+            type="text"
+            id="name"
+            register={register}
+            condition={{ required: "This field is required" }}
+            disabled={isPending}
+            placeholder="Enter project name" />
           <div className="h-5 text-danger">
             {errors?.name?.message && <p>{errors.name.message}</p>}
           </div>
         </FormRow>
         <FormRow>
           <Label htmlFor="description">Notes</Label>
-          <Textarea rows={5} id="description" register={register} disabled={isPending} />
+          <Textarea
+            rows={5}
+            id="description"
+            register={register}
+            condition={{ required: "This field is required" }}
+            disabled={isPending}
+            placeholder="Enter important information about the project" />
           <div className="h-5 text-danger">
             {errors?.description?.message && <p>{errors.description.message}</p>}
           </div>
@@ -50,24 +66,47 @@ export default function CreateProjectForm({ onCloseModal }) {
         <div className="grid grid-cols-4 gap-4">
           <FormRow className="col-span-2">
             <Label htmlFor="startDate">Start Date</Label>
-            <Input type="date" id="startDate" register={register} disabled={isPending} />
+            <Input
+              type="date"
+              id="startDate"
+              register={register}
+              condition={{ required: "This field is required" }}
+              disabled={isPending}
+              placeholder="Enter start date" />
             <div className="h-5 text-danger">
-            {errors?.startDate?.message && <p>{errors.startDate.message}</p>}
-          </div>
+              {errors?.startDate?.message && <p>{errors.startDate.message}</p>}
+            </div>
           </FormRow>
           <FormRow className="col-span-2">
             <Label htmlFor="finishDate">Finish Date</Label>
-            <Input type="date" id="finishDate" register={register} disabled={isPending} />
+            <Input
+              type="date"
+              id="finishDate"
+              register={register}
+              condition={{ required: "This field is required" }}
+              disabled={isPending}
+              placeholder="Enter end date" />
             <div className="h-5 text-danger">
-            {errors?.finishDate?.message && <p>{errors.finishDate.message}</p>}
-          </div>
+              {errors?.finishDate?.message && <p>{errors.finishDate.message}</p>}
+            </div>
           </FormRow>
         </div>
         <FormRow>
           <Label htmlFor="budget">Budget</Label>
-          <Input type="number" id="budget" min={1} register={register} disabled={isPending} />
+          <Input
+            type="number"
+            id="budget"
+            min={1}
+            disabled={isPending || checked}
+            register={register}
+            condition={{ required: checked ? false : "This field is required" }}
+            placeholder="Enter project budget" />
           <div className="h-5 text-danger">
-            {errors?.name?.message && <p>{errors.budget.message}</p>}
+            {errors?.budget?.message && <p>{errors.budget.message}</p>}
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
+            <Label>No Budget</Label>
           </div>
         </FormRow>
         <Button disabled={isPending} type="submit">Add project</Button>
