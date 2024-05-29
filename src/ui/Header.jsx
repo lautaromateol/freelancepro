469b69dd780cb4c3db308ebc61cdc5395128933c
lautaromateol <lautaromateol@gmail.com>
2024-05-29 16:58:04 -0300

    show email for example user at header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
 
   return (
     <header className="flex items-center justify-end gap-4 px-16 bg-slate-50 h-[8rem] border-b border-b-1">
-      <p className="text-2xl text-primary">{user.user_metadata.fullName}</p>
+      <p className="text-2xl text-primary">{user.user_metadata.fullName || user.user_metadata.email}</p>
       <button onClick={signOut} disabled={isPending}>
         <HiArrowTopRightOnSquare className="text-4xl text-primary hover:text-tint" />
       </button>
