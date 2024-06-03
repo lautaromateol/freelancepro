bb052809dad3695b40a83df2c0119fa30ea86da0
lautaromateol <lautaromateol@gmail.com>
2024-06-03 15:08:38 -0300

    carousel added to dashboard

--- src/features/dashboard/ProjectsStatistics.jsx
+++ src/features/dashboard/ProjectsStatistics.jsx
@@ -1,21 +1,42 @@
 import { HiOutlineArchiveBox, HiOutlineCalendar, HiOutlineCheckCircle, HiOutlineExclamationTriangle } from "react-icons/hi2";
-import SummaryChart from "./SummaryChart";
 import { isAfter, isBefore, isToday, startOfDay } from "date-fns";
+import SummaryChart from "./SummaryChart";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
 
 /* eslint-disable react/prop-types */
 export default function ProjectsSummary({ projects }) {
 
-  const active = projects.filter(({status}) => status === "In Progress").length
-  const completed = projects.filter(({status}) => status === "Complete").length
-  const overdue = projects.filter(({status, finishDate}) => status === "In Progress" && isAfter(startOfDay(new Date()), startOfDay(new Date(finishDate)))).length
-  const upcoming = projects.filter(({status, finishDate}) => status === "In Progress" && isBefore(startOfDay(new Date()), startOfDay(new Date(finishDate))) || isToday(new Date(finishDate))).length
+  const active = projects.filter(({ status }) => status === "In Progress").length
+  const completed = projects.filter(({ status }) => status === "Complete").length
+  const overdue = projects.filter(({ status, finishDate }) => status === "In Progress" && isAfter(startOfDay(new Date()), startOfDay(new Date(finishDate)))).length
+  const upcoming = projects.filter(({ status, finishDate }) => status === "In Progress" && isBefore(startOfDay(new Date()), startOfDay(new Date(finishDate))) || isToday(new Date(finishDate))).length
 
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      <SummaryChart title="Active Projects" icon={<HiOutlineArchiveBox />} data={active} />
-      <SummaryChart title="Completed Projects" icon={<HiOutlineCheckCircle />} data={completed} />
-      <SummaryChart title="Overdue Projects" icon={<HiOutlineExclamationTriangle />} data={overdue} />
-      <SummaryChart title="Upcoming Projects" icon={<HiOutlineCalendar />} data={upcoming} />
-    </section>
+    <>
+      <section className="hidden md:grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <SummaryChart title="Active Projects" icon={<HiOutlineArchiveBox />} data={active} />
+        <SummaryChart title="Completed Projects" icon={<HiOutlineCheckCircle />} data={completed} />
+        <SummaryChart title="Overdue Projects" icon={<HiOutlineExclamationTriangle />} data={overdue} />
+        <SummaryChart title="Upcoming Projects" icon={<HiOutlineCalendar />} data={upcoming} />
+      </section>
+      <section className="md:hidden">
+        <Slider {...settings}>
+          <SummaryChart title="Active Projects" icon={<HiOutlineArchiveBox />} data={active} />
+          <SummaryChart title="Completed Projects" icon={<HiOutlineCheckCircle />} data={completed} />
+          <SummaryChart title="Overdue Projects" icon={<HiOutlineExclamationTriangle />} data={overdue} />
+          <SummaryChart title="Upcoming Projects" icon={<HiOutlineCalendar />} data={upcoming} />
+        </Slider>
+      </section>
+    </>
   )
 }
