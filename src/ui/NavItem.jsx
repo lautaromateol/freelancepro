e6c4c209578f0680725ff82c0035ef044a5bb30c
lautaromateol <lautaromateol@gmail.com>
2024-06-03 14:13:50 -0300

    routing error fixed

--- src/ui/NavItem.jsx
+++ src/ui/NavItem.jsx
@@ -3,9 +3,8 @@ import { NavLink } from "react-router-dom"
 
 export default function NavItem({children, to, setIsOpen}) {
   return (
-      <li className="text-tremor-default">
+      <li onClick={() => setIsOpen(false)} className="text-tremor-default">
         <NavLink
-          onClick={() => setIsOpen(false)}
           className={({ isActive }) => {
             return `${isActive ? "text-tremor-brand-emphasis dark:text-dark-tremor-brand-emphasis" : "text-tremor-brand dark:text-dark-tremor-brand"} text-tremor-title md:text-tremor-default`
           }}
