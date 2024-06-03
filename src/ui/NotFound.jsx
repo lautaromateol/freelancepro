38a470c5866b506072b56ff7bbad335af7e01749
lautaromateol <lautaromateol@gmail.com>
2024-06-03 08:13:21 -0300

    not found page

--- src/ui/NotFound.jsx
+++ src/ui/NotFound.jsx
@@ -1,5 +1,18 @@
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <main className="flex items-center justify-center min-h-screen">
+      <section className="flex flex-col gap-2">
+        <h2 className="font-bold text-center text-tremor-metric text-tremor-content-strong dark:text-dark-tremor-content-strong">404</h2>
+        <h2 className="text-tremor-title text-center text-tremor-content-strong dark:text-dark-tremor-content-strong">Page not Found!</h2>
+        <Link to="/dashboard">
+          <Button>
+            Back to Dashboard
+          </Button>
+        </Link>
+      </section>
+    </main>
   )
 }
