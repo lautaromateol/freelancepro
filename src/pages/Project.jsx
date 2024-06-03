f0e47cbac3273c57eb3941bd172f1c562fa75000
lautaromateol <lautaromateol@gmail.com>
2024-06-03 08:28:37 -0300

    project not found error handling

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,8 @@
 import { useProject } from "../features/projects/useProject"
 import { useTasks } from "../features/tasks/useTasks"
+import { useUser } from "../features/auth/useUser"
+import { HiOutlineExclamationTriangle } from "react-icons/hi2"
+import { Link } from "react-router-dom"
 import Spinner from "../ui/Spinner"
 import PageSection from "../ui/PageSection"
 import PageHeading from "../ui/PageHeader"
@@ -10,7 +13,7 @@ import CreateExpense from "../features/expenses/CreateExpense"
 import EditProjectButton from "../features/projects/EditProjectButton"
 import DeleteProjectButton from "../features/projects/DeleteProjectButton"
 import ProjectDescription from "../features/projects/ProjectDescription"
-import { useUser } from "../features/auth/useUser"
+import Button from "../ui/Button"
 
 export default function Project() {
 
@@ -23,7 +26,20 @@ export default function Project() {
   const isPending = isLoadingProjects || isLoadingTasks || isLoadingUser
 
   if (isPending) return <Spinner />
-  
+
+  if (!project) return (
+    <section className="flex items-center justify-center">
+      <div className="rounded-md bg-tremor-background shadow-md p-4 max-w-[20rem]">
+        <HiOutlineExclamationTriangle className="mx-auto size-[2rem] mb-6" />
+        <h4 className="font-semibold text-tremor-title text-tremor-content-strong dark:text-dark-tremor-content-strong mb-2 text-center">Project not found!</h4>
+        <p className="text-text-tremor-default mb-2 text-tremor-content dark:text-dark-tremor-content text-center">The project that you were looking for was not found at our database.</p>
+        <Link to="/projects">
+          <Button>Back to Projects</Button>
+        </Link>
+      </div>
+    </section>
+  )
+
   return (
     <PageSection>
       <header className="flex items-center justify-between bg-tremor-background dark:bg-dark-tremor-background shadow-md rounded-md border border-1 p-4">
