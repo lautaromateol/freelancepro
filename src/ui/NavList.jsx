25330445916532f906bad4d225b4e501928d7e18
lautaromateol <lautaromateol@gmail.com>
2024-06-03 14:24:22 -0300

    dark mode added to mobile menu

--- src/ui/NavList.jsx
+++ src/ui/NavList.jsx
@@ -26,7 +26,7 @@ export default function NavList({ setIsOpen }) {
           </NavItem>
         ))}
       </ul>
-      <ul className="md:hidden flex flex-col items-center justify-center gap-12 min-h-screen w-full bg-tremor-background-muted">
+      <ul className="md:hidden flex flex-col items-center justify-center gap-12 min-h-screen w-full bg-tremor-background-muted dark:bg-dark-tremor-background-muted">
         {pages.map(({ name, to }) => (
           <NavItem setIsOpen={setIsOpen} to={to} key={name}>
             {name}
